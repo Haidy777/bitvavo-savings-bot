6e352a3c125464f987f9aa625c022dca3ca593a4
Philipp Haidenbauer <5011101+Haidy777@users.noreply.github.com>
2022-11-29 07:31:32 +0100

    Remove Near & Dot as they are not withdrawable yet. :(

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 A simple script to automatically buy crypto and transfer it to wallets on a regular basis.
 
+Withdrawals only work if the Address is Added to the Allowlist on Bitvavo.
+
 TODO :)
--- src/config/withdrawals.ts
+++ src/config/withdrawals.ts
@@ -7,19 +7,7 @@ export interface IWithdrawConfigEntry {
 export const WITHDRAWAL_CONFIG: IWithdrawConfigEntry[] = [
   {
     symbol: 'ETH',
-    cron: '1 1 * * TUE',
+    cron: '1 2 * * *',
     address: '0x9b41030A0A009c4025bdEBe1b44Fa4b2AA1bdf54',
-  },
-
-  {
-    symbol: 'DOT',
-    cron: '1 1 * * TUE',
-    address: '15twEe9iUMXwwCwavGA5Ja9tiGxffBV1DcSKomqkGn7XtrCq',
-  },
-
-  {
-    symbol: 'NEAR',
-    cron: '1 1 * * TUE',
-    address: '92598ffa91508ec62b827aae60691c34c4cfb2dcba83b16acd4380ce16856110',
   },
 ];
