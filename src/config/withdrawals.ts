77d1c21bf0db3f9539a241594c95b26023242f9e
Philipp Haidenbauer <5011101+Haidy777@users.noreply.github.com>
2023-01-04 16:11:52 +0100

    Only Monthly withdrawals

--- src/config/withdrawals.ts
+++ src/config/withdrawals.ts
@@ -7,13 +7,13 @@ export interface IWithdrawConfigEntry {
 export const WITHDRAWAL_CONFIG: IWithdrawConfigEntry[] = [
   {
     symbol: 'ETH',
-    cron: '1 2 * * *',
+    cron: '1 2 15 * *',
     address: '0x9b41030A0A009c4025bdEBe1b44Fa4b2AA1bdf54',
   },
 
   {
     symbol: 'DOT',
-    cron: '1 2 * * *',
+    cron: '1 2 15 * *',
     address: '15twEe9iUMXwwCwavGA5Ja9tiGxffBV1DcSKomqkGn7XtrCq',
   },
 ];
